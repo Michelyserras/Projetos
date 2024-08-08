fa3af30d6ccef46e53fa3d1bd9dc9f7e47f8b6bb
Michelyserras <michelysrrs@gmail.com>
2024-08-08 16:19:01 -0300

    controller cliente - quase feito

--- Projeto_Interdisciplinar/src/controller/ClienteController.ts
+++ Projeto_Interdisciplinar/src/controller/ClienteController.ts
@@ -1,59 +1,57 @@
 import { Request, Response } from "express";
 import { ClienteService } from "../service/ClienteService";
-import { Controller, Route, Body, Res, Tags, TsoaResponse, Post, Get, Put} from "tsoa";
+import { Controller, Route, Body, Res, Tags, TsoaResponse, Post, Get, Put, delete} from "tsoa";
 import { ProductRequestDto } from "../model/dto/ProductRequestDto";
 import { BasicResponseDto } from "../model/dto/BasicResponseDto";
 // aaaaaaaaaaa
 @Route("product")
 @Tags("Product")
 export class ProductController{ 
-    productService = new ClienteService();
+    clienteService = new ClienteService();
     
     @Post()
-    async cadastrarProduto(
+    async cadastrarCliente(
         @Body() dto:ProductRequestDto,
         @Res() fail: TsoaResponse<400, BasicResponseDto>,
         @Res() sucess: TsoaResponse<201, BasicResponseDto>
     ): Promise<void> {
             try {
-                const novoProduto =  await this.productService.cadastrarProduto(dto);
-               return sucess(201, new BasicResponseDto("Produto criado ccom sucesso!", novoProduto));
+                const novocliente =  await this.clienteService.cadastrarCliente(dto);
+               return sucess(201, new BasicResponseDto("Cliente cadastrado com sucesso!", novocliente));
             } catch (error: any) {
                 return fail(400, new BasicResponseDto(error.message, undefined));
             }
     };
         
     
-
-    async atualizarProduto (req: Request, res: Response){
+    @Put()
+    async atualizarCliente(
+        @Body() dto:ProductRequestDto,
+        @Res() fail:TsoaResponse<400, BasicResponseDto>,
+        @Res() sucess: TsoaResponse<200, BasicResponseDto>
+    ): Promise<void>{
             try {
-                const produto = await this.productService.atualizarProduto(req.body);
-                res.status(200).json(
-                    {
-                        mensagem:"Produto atualizado com sucesso!",
-                        produto:produto
-                    }
-                );
+                const clienteAtlz = await this.clienteService.atualizarCliente(dto);
+                return sucess(200, new BasicResponseDto("Os dados do Cliente foram atualizado com sucesso!", clienteAtlz));
             } catch (error: any) {
-                res.status(400).json({ message: error.message});
+                return fail(400, new BasicResponseDto(error.message, undefined));
             }
         };
         
     
-
-    async deletarProduto (req: Request, res: Response){
-            try {
-                const produto = await this.productService.deletarProduto(req.body);
-                res.status(200).json(
-                    {
-                        mensagem:"Produto deletado com sucesso!",
-                        produto:produto
-                    }
-                );
-            } catch (error: any) {
-                res.status(400).json({ message: error.message});
-            }
-        };
+    @Delete()
+    async deleteCliente(
+        @Body() dto:ProductRequestDto,
+        @Res() fail:TsoaResponse<400, BasicResponseDto>,
+        @Res() sucess: TsoaResponse<200, BasicResponseDto>
+    ):Promise<void>{
+        try {
+            const cliente = await this.clienteService.deletarCliente(dto);
+            return sucess(200, new BasicResponseDto("Cliente excluido com suceso!", cliente));
+        } catch (error: any) {
+            return fail(400, new BasicResponseDto(error.message, undefined));
+        }
+    };
         
     
 
