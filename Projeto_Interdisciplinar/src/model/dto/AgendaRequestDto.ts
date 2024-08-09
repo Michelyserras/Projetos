c7dc6d8d673d48371571d56cb81910a7bcca6c8a
MaluAbrami <marialuizaabrami@gmail.com>
2024-08-09 20:49:45 -0300

    alteração

--- Projeto_Interdisciplinar/src/model/dto/AgendaRequestDto.ts
+++ Projeto_Interdisciplinar/src/model/dto/AgendaRequestDto.ts
@@ -4,15 +4,15 @@ export class AgendaRequestDto {
     hora: number; //VERIFICAR DEPOIS O FORMATO DA HORA
     tipoServico: string;
     valorServico: number;
-    cpfCliente: number;
+    cpfCliente: string;
     idPet: number;
 
-    constructor(data?:string, hora?:number, tipoServico?:string, valorServico?:number, cpfCliente?:number, idPet?:number){
+    constructor(data?:string, hora?:number, tipoServico?:string, valorServico?:number, cpfCliente?:string, idPet?:number){
         this.data = stringParaData(data || '');
         this.hora = hora || 0;
         this.tipoServico = tipoServico || '';
         this.valorServico = valorServico || 0;
-        this.cpfCliente = cpfCliente || 0;
+        this.cpfCliente = cpfCliente || '';
         this.idPet = idPet || 0;
     }
 
