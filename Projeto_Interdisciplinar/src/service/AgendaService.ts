0c976b920c6290e46474b137940071d0d9fdff68
Malu Abrami <marialuizaabrami@gmail.com>
2024-08-09 16:40:17 -0300

    adicionando validações para service agenda e adicionando comentários sobre possíveis funções

--- Projeto_Interdisciplinar/src/service/AgendaService.ts
+++ Projeto_Interdisciplinar/src/service/AgendaService.ts
@@ -11,30 +11,52 @@ export class AgendaService{
     async cadastrarAgenda(agendaData: any): Promise<AgendaEntity> { //Ao cadastrar um agendamento é necessário verificar qual o tipo de serviço escolhido para buscarmos na tabela Serviço se existe e descobrir o valor
         const { data, hora, tipoServico, cpfCliente, idPet } = agendaData;
         const verificaServico = await this.verificarTipoServico(tipoServico);
+        const agendaEncontrada = await this.filtrarAgenda(agendaData);//USAR AQUI A NOVA FUNÇÃO PARA VERIFICAR SE O AGENDAMENTO JÁ EXISTE NA DATA E HORA
 
-        if(!verificaServico){
-            throw new Error('Tipo de serviço inexistente.');
+        if(agendaEncontrada){ //PRECISA FAZER UMA FUNÇÃO PARA PODERMOS VERIFICAR SE JÁ HÁ O AGENDAMENTO NA DATA E HORA ESCOLHIDA
+            throw new Error('Já há um agendamento nesta data e hora.');
         }
-        
-        const agenda = new AgendaEntity(undefined, data, hora, tipoServico, cpfCliente, idPet);
-
-        const novaAgenda =  await this.agendaRepository.insertAgenda(agenda);
-        console.log("Service - Insert ", novaAgenda);
-        return novaAgenda;
+        else{
+            if(!verificaServico){
+                throw new Error('Tipo de serviço inexistente.');
+            }
+            
+            const agenda = new AgendaEntity(undefined, data, hora, tipoServico, cpfCliente, idPet);
+    
+            const novaAgenda =  await this.agendaRepository.insertAgenda(agenda);
+            console.log("Service - Insert ", novaAgenda);
+            return novaAgenda;
+        }
     }
 
     async atualizarAgenda(agendaData: any): Promise<AgendaEntity> { //Ao atualizar agenda deve ser possível trocar o tipo de serviço escolhido durante o cadastro
         const { id, data, hora, tipoServico, cpfCliente, idPet } = agendaData;
+        const verificaServico = await this.verificarTipoServico(tipoServico);
+        const agendaEncontrada = await this.filtrarAgenda(agendaData);//USAR AQUI A NOVA FUNÇÃO PARA VERIFICAR SE O AGENDAMENTO JÁ EXISTE NA DATA E HORA
 
-        const agenda = new AgendaEntity(id, data, hora, tipoServico, cpfCliente, idPet);
-
-        await this.agendaRepository.updateAgenda(agenda);
-        console.log("Service - Update ", agenda);
-        return agenda;
+        if(agendaEncontrada){ //PRECISA FAZER UMA FUNÇÃO PARA PODERMOS VERIFICAR SE JÁ HÁ O AGENDAMENTO NA DATA E HORA ESCOLHIDA
+            throw new Error('Já há um agendamento nesta data e hora.');
+        }
+        else{
+            if(!verificaServico){
+                throw new Error('Tipo de serviço inexistente.');
+            }
+    
+            const agenda = new AgendaEntity(id, data, hora, tipoServico, cpfCliente, idPet);
+    
+            await this.agendaRepository.updateAgenda(agenda);
+            console.log("Service - Update ", agenda);
+            return agenda;
+        }
     }
 
     async deletarAgenda(agendaData: any): Promise<AgendaEntity> { 
         const { id, data, hora, tipoServico, cpfCliente, idPet } = agendaData;
+
+        const agendaEncontrada = await this.filtrarAgenda(agendaData);
+        if(!agendaEncontrada){
+            throw new Error('Agendamento não encontrado.');
+        }
 
         const agenda = new AgendaEntity(id, data, hora, tipoServico, cpfCliente, idPet);
 
@@ -56,8 +78,13 @@ export class AgendaService{
     }
 
     async verificarTipoServico(tipoServico: string): Promise<ServicoEntity>{
-        const servico = await this.servicoRepository.verificaTipoServico();
+        const servico = await this.servicoRepository.verificaTipoServico(tipoServico);
         console.log("Service - Verifica serviço", servico);
         return servico;
     }
+
+    //GERAR FATURAMENTO SERIA SOMAR O VALOR DE TODOS OS SERVIÇOS
+    //CRIAR FUNÇÃO GERAR FATURA TOTAL DA AGENDA
+    //CRIAR FUNÇÃO GERAR FATURA POR CPF CLIENTE
+    //CRIAR FUNÇÃO GERAR FATURA POR PET ID
 }
