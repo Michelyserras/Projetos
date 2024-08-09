57cdcec7b7f663e4496ce49226afe5a5eeae0b38
Michelyserras <Michelysrrs@gmail.com>
2024-08-09 19:17:25 -0300

    mudei tipoServico para idServico - feito

--- Projeto_Interdisciplinar/src/model/entity/Agenda.ts
+++ Projeto_Interdisciplinar/src/model/entity/Agenda.ts
@@ -4,26 +4,26 @@ export class AgendaEntity{
     id: number;
     data: Date;
     hora: number; //VERIFICAR DEPOIS O FORMATO DA HORA
-    tipoServico: string; //FOREIGN KEY
+    idServico: number; //FOREIGN KEY
     cpfCliente: number; //FOREIGN KEY
     idPet: number; //FOREIGN KEY
 
 //CONFIRMAR SE ESSA ENTIDADE ESTÁ CERTA, A IDEIA É ARMAZENAR DADOS DO PET, CLIENTE E DO SERVIÇO REALIZADO NA DATA E HORA MARCADA
 
-    constructor(id?:number, data?:string, hora?:number, tipoServico?:string, cpfCliente?:number, idPet?:number){
-        this.validatesInformation(data, hora, tipoServico)
+    constructor(id?:number, data?:string, hora?:number, idServico?:number, cpfCliente?:number, idPet?:number){
+        this.validatesInformation(data, hora, idServico)
         this.id = id || 0;
         this.data = stringParaData(data || '');
         this.hora = hora || 0;
-        this.tipoServico = tipoServico || '';
+        this.idServico = idServico || 0;
         this.cpfCliente = cpfCliente || 0;
         this.idPet = idPet || 0;
     }
 
-    private validatesInformation(data:any, hora:any, tipoServico:any){
+    private validatesInformation(data:any, hora:any, idServico:any){
         let error = '';
 
-        if(typeof data !== 'string' || typeof hora !== 'number' || typeof tipoServico !== 'string'){
+        if(typeof data !== 'string' || typeof hora !== 'number' || typeof idServico !== 'number'){
             error += ("Informaçõe incompletas ou incorretas.");
         }
 
