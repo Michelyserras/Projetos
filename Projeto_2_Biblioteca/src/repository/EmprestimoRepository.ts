99e7f5466dba894a9d33a60de9624d03b87c0207
Michelyserras <michelysrrs@gmail.com>
2024-08-15 11:22:32 -0300

    adicionando metodos na classe EmprestimoRepository

--- Projeto_2_Biblioteca/src/repository/EmprestimoRepository.ts
+++ Projeto_2_Biblioteca/src/repository/EmprestimoRepository.ts
@@ -52,4 +52,70 @@ export class EmprestimoRepository{
         }
     }
 
+    async atualizarEmprestimo(Emprestimo: EmprestimoEntity): Promise<EmprestimoEntity>{
+        const query = 'UPDATE biblioteca.Emprestimos SET livroId = ?, usuarioId = ?, dataEmprestimo = ?, dataDevolucao = ? WHERE id = ?;';
+
+        try{
+            const resultado = await executarComandoSQL(query, [Emprestimo.livroId, Emprestimo.usuarioId, Emprestimo.dataEmprestimo, Emprestimo.dataDevolucao]);
+            console.log("Emprestimo atualizado com sucesso:", resultado);
+            return new Promise<EmprestimoEntity>((resolve)=>{
+                resolve(Emprestimo);
+            })
+        }catch(err: any){
+            console.error('Erro ao atualizar os dados da Emprestimo', err);
+            throw err;
+        }
+    }
+
+    async filtrarEmprestimoPorId(id: number): Promise<EmprestimoEntity | null>{
+        const query = 'SELECT * FROM biblioteca.Emprestimos WHERE id = ?';
+        try{
+            const resultado = await executarComandoSQL(query,[id]);
+            if(resultado.length > 0){
+                console.log("Emprestimo localizado com sucesso:", resultado);
+                return resultado;
+            }
+            else{
+                return null;
+            }
+        }catch(err: any){
+            console.error(`Erro ao localizar Emprestimo com id = ${id} na base de dados`, err);
+            throw err;
+        }
+    }
+
+    async listarTodasEmprestimos(): Promise<EmprestimoEntity[] | null>{
+        const query = 'SELECT * FROM biblioteca.Emprestimos;';
+
+        try{
+            const resultado = await executarComandoSQL(query, []);
+            if(resultado.length > 0){
+                console.log("Emprestimos realizados: ", resultado);
+                return resultado;
+            }
+            else{
+                return null;
+            }
+        }catch(err: any){
+        console.error(`Erro ao listar Emprestimos realizados!`, err);
+        throw err;
+        }
+    }
+
+    async deletarEmprestimo(id: number): Promise<EmprestimoEntity>{
+        const query = 'DELETE * FROM biblioteca.Emprestimos WHERE id = ?;';
+        try{
+
+            const resultado = await executarComandoSQL(query,[id]);
+            console.log(`O Emprestimo com o id = ${id} foi deletada com sucesso!`, resultado);
+            return new Promise<EmprestimoEntity>((resolve)=>{
+                resolve(resultado);
+            });
+
+        }catch (err: any){
+            console.error(`Erro ao deletar Emprestimo com id = ${id}`, err);
+            throw err;
+        }
+    }
+
 }
