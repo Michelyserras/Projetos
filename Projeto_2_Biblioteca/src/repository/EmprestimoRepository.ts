77e7b9f929973c0ab314962a42245a7051cd0877
Michelyserras <Michelysrrs@gmail.com>
2024-08-14 22:08:42 -0300

    adicionando funcao para realizar um emprestimo

--- Projeto_2_Biblioteca/src/repository/EmprestimoRepository.ts
+++ Projeto_2_Biblioteca/src/repository/EmprestimoRepository.ts
@@ -22,8 +22,11 @@ export class EmprestimoRepository{
         CREATE TABLE IF NOT EXISTS biblioteca.Emprestimo (
             id INT AUTO_INCREMENT PRIMARY KEY,
             livroId INT NOT NULL,
-            price DECIMAL(10,2) NOT NULL,
-            expirationDate DATE NOT NULL
+            usuarioId INT NOT NULL,
+            dataEmprestimo DATE NOT NULL,
+            dataDevolucao DATE NOT NULL,
+            FOREIGN KEY (livroId) REFERENCES biblioteca.Livros(id),
+            FOREIGN KEY (usuarioId) REFERENCES biblioteca.Usuarios(id)
         )`;
 
         try {
@@ -31,6 +34,21 @@ export class EmprestimoRepository{
                 console.log('Query executada com sucesso:', resultado);
         } catch (err) {
             console.error('Error');
+        }
+    }
+
+    async realizarEmprestimo(Emprestimo: EmprestimoEntity): Promise<EmprestimoEntity>{
+        const query = 'INSERT INTO biblioteca.Emprestimo(livroId, usuarioId, dataEmprestimo, dataDevolucao) VALUES (?,?,?,?)'
+        try{
+            const resultado = await executarComandoSQL(query, [Emprestimo.livroId, Emprestimo.usuarioId, Emprestimo.dataEmprestimo, Emprestimo.dataDevolucao]);
+            console.log("Emprestimo realizado com sucesso!", resultado);
+            Emprestimo.id = resultado.insertId;
+            return new Promise<EmprestimoEntity>((resolve)=>{
+                resolve(Emprestimo);
+            })
+        } catch (err) {
+            console.error('Erro ao cadastrar a agenda:', err);
+            throw err;
         }
     }
 
