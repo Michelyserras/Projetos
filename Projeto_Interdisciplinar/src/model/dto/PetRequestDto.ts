1c2a9bfd2741a6c68acf52e72328656638fd79be
Malu Abrami <marialuizaabrami@gmail.com>
2024-08-21 11:53:44 -0300

    tirando peso do pet do dto

--- Projeto_Interdisciplinar/src/model/dto/PetRequestDto.ts
+++ Projeto_Interdisciplinar/src/model/dto/PetRequestDto.ts
@@ -2,15 +2,13 @@ export class PetRequestDto{
     cpfCliente: string;
     nome: string;
     idade: number;
-    peso: number; //ALTERAR NO DIAGRAMA DE CLASSES, colocar o peso e tirar a raça do pet
     especie: string;
     historicoPet: string;
 
-    constructor( cpfCliente?:string, nome?:string, idade?:number, peso?:number, especie?:string, historicoPet?:string){
+    constructor( cpfCliente?:string, nome?:string, idade?:number, especie?:string, historicoPet?:string){
         this.cpfCliente = cpfCliente || '';
         this.nome = nome || '';
         this.idade = idade || 0;
-        this.peso = peso || 0;
         this.especie = especie || '';
         this.historicoPet = historicoPet || '';
     }
