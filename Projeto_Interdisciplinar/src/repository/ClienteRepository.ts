7c286c7e3760f6debb106a6e6a5ba600ca38d94c
Malu Abrami <marialuizaabrami@gmail.com>
2024-08-21 11:06:45 -0300

    alterando varchar telefone

--- Projeto_Interdisciplinar/src/repository/ClienteRepository.ts
+++ Projeto_Interdisciplinar/src/repository/ClienteRepository.ts
@@ -22,7 +22,7 @@ export class ClienteRepository{
             cpf VARCHAR(14) PRIMARY KEY,
             nome VARCHAR(255) NOT NULL,
             endereco VARCHAR(255) NOT NULL,
-            telefone VARCHAR(12) NOT NULL
+            telefone VARCHAR(45) NOT NULL
         )`;
 
         try {
